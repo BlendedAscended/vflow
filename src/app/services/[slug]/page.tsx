2e46399d86d1f077fb86934fe30a986479f1ebf0
Sandeep Singh <sandeepthealchemist@gmail.com>
2025-09-23 02:04:04 -0400

    portabletextblock removed any

--- src/app/services/[slug]/page.tsx
+++ src/app/services/[slug]/page.tsx
@@ -150,7 +150,7 @@ export default async function ServicePage({ params }: ServicePageProps) {
         <section className="py-24 bg-white">
           <div className="max-w-4xl mx-auto px-6 lg:px-12">
             <div className="prose prose-lg max-w-none">
-              <PortableText value={(service.fullDescription as any) ?? [] } />
+            <PortableText value={(service.fullDescription ?? []) as PortableTextBlock[]} />
             </div>
           </div>
         </section>
